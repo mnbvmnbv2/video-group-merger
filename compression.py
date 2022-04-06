05b288726bc41e262a5b0f0fab4947fe41703233
mnbvmnbv2 <ferdinand@ytteborg.com>
2022-04-06 19:39:10 +0200

    Comment and fix addition of :

--- compression.py
+++ compression.py
@@ -11,12 +11,19 @@ def compress_file(filepath):
     
 def compress(df):
     for i in range(df.shape[0]):
+        #Check every video in dataframe if it is comrpessed
         if df.iloc[i, df.columns.get_loc('Compressed')] == False:
+            # runs the compression of the time
             compress_file(df.iloc[i, df.columns.get_loc('Directory')] + '/' + df.iloc[i, df.columns.get_loc('File Name')])
+
             df.iloc[i, df.columns.get_loc('Compressed')] = True
+
+            #Gets the current time
             mod_time = time.strftime("%Y:%m:%d %H:%M:%S%z", time.gmtime())
-            mod_time = ":".join(mod_time.split("00", 1)) + '00'
+            #add : to the local time difference to get correct format
+            mod_time = mod_time[:-2] + ':' + mod_time[-2:]
             df.iloc[i, df.columns.get_loc('File Modification Date/Time')] = mod_time
+            #update the csv after each compression
             df.to_csv('data.csv', index=False)
 
 df = pd.read_csv('data.csv')
