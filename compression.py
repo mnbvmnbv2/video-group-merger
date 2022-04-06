158c803674bc52bbf033ddee043590265fa33030
mnbvmnbv2 <ferdinand@ytteborg.com>
2022-04-06 17:56:41 +0200

    Change compression to be based on metadata file

--- compression.py
+++ compression.py
@@ -1,22 +1,19 @@
-import glob
 import shutil
 import subprocess
-import os
-from concurrent.futures import ThreadPoolExecutor
+import pandas as pd
 
 crf = 34
 
-def compress(filepath):
-	subprocess.run(['ffmpeg', '-i', filepath, '-vcodec', 'libx264', '-preset', 'veryfast', '-crf', str(crf), filepath+'cpr.mp4'], text=True, input="y")
-	shutil.move(filepath+'cpr.mp4', filepath)
-	os.remove(filepath+'cpr.mp4')
+def compress_file(filepath):
+    subprocess.run(['ffmpeg', '-i', filepath, '-vcodec', 'libx264', '-preset', 'veryfast', '-crf', str(crf), filepath+'cpr.mp4'], text=True, input="y")
+    shutil.move(filepath+'cpr.mp4', filepath)
+    
+def compress(df):
+    for i in range(df.shape[0]):
+        if df.iloc[i, df.columns.get_loc('Compressed')] == False:
+            compress_file(df.iloc[i, df.columns.get_loc('Directory')] + '/' + df.iloc[i, df.columns.get_loc('File Name')])
+            df.iloc[i, df.columns.get_loc('Compressed')] = True
+            df.to_csv('data.csv')
 
-channels = glob.glob('Channels/*')
-for channel in channels:
-	#iterates all videos in all channels
-	vids = glob.glob(channel+'/*.mp4')
-	#sort by date
-	vids.sort(key=os.path.getmtime)
-
-	with ThreadPoolExecutor(max_workers=1) as exe:
-		exe.map(compress,vids)
+df = pd.read_csv('data.csv')
+compress(df)
