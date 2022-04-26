1648523154efebe29b00c8d93b88fbfd8f940374
mnbvmnbv2 <ferdinand@ytteborg.com>
2022-04-26 15:48:57 +0200

    Cleanup mergers.py

--- mergers.py
+++ mergers.py
@@ -2,13 +2,6 @@ import moviepy.editor
 import glob
 import os
 
-do_compression = False
-do_threading = False
-
-
-#added to compressed videofiles
-suffix = '-d.mp4'
-
 channels = glob.glob('Channels/*')
 for channel in channels:
     #iterates all videos in all channels
@@ -27,4 +20,3 @@ for channel in channels:
     
     final = moviepy.editor.concatenate_videoclips(processed_vids)
     final.write_videofile(channel+"All.mp4")
-    #final.ipython_display()
