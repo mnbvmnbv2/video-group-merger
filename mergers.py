845c17219b45c86d70ab2ec9bf8d8e0eddf380ff
mnbvmnbv2 <ferdinand@ytteborg.com>
2023-05-22 03:28:18 +0200

    fix merger with cuda

--- mergers.py
+++ mergers.py
@@ -13,7 +13,7 @@ def save_flist(videos: list) -> None:
     Args:
         videos (list): List of videos
     """
-    f_data = "file " + "\nfile ".join(videos)
+    f_data = "file '" + "'\nfile '".join(videos) + "'"
 
     f_list = "temp/list.txt"
     with open(f_list, "w", encoding="UTF-8") as f:
@@ -37,11 +37,6 @@ def merge_videos(path: str) -> None:
     # load in json as dictionary
     mergersdata = json.load(f)
 
-    # edit path to FFMPEG format
-    FFM_path = path.replace("\\", "\\\\")
-    FFM_path = FFM_path.replace(" ", "\\ ")
-    FFM_path = FFM_path.replace("'", "\\'")
-
     # iterate over group (videos)
     for idx, group in enumerate(mergersdata):
         # print name and which group out of total number of group is being processed
@@ -54,7 +49,7 @@ def merge_videos(path: str) -> None:
             merged = False  # if the key does not exist it has not been merged yet
         # if the group is merged then continue
         if merged:
-            pass
+            continue
         # get videos in group
         videos = group["videos"]
 
@@ -66,31 +61,26 @@ def merge_videos(path: str) -> None:
         processed_videos = []
         # iterate videos
         for video in videos:
-            print(video)
-            # edit video name to FFMPEG format
-            # video_name = video.replace("\\", "\\\\")
-            # video_name = video_name.replace(" ", "\\ ")
-            # video = video_name.replace("'", "\\'")
-
             # get original video path
             orig_video_path = path + "\\" + video
-            print(orig_video_path)
             # set output video path
-            video_path = f"temp\\{video[:-4]}.mkv"
+            out_video_path = f"temp\{video[:-4]}.mp4"
 
-            ffmpeg_command = f'ffmpeg -y -i "{orig_video_path}" -max_interleave_delta 0 -vf "setpts=1.25*PTS, fps=15" -r 15 "{video_path}"'
+            # ffmpeg_command = f'ffmpeg -y -i "{orig_video_path}" -max_interleave_delta 0 -vf "setpts=1.25*PTS, fps=15" -r 15 "{video_path}"'
+            ffmpeg_command = f'ffmpeg -y -i "{orig_video_path}" -max_interleave_delta 0 -vf "setpts=1*PTS, fps=20" -c:v h264_nvenc -r 15 "{out_video_path}"'
             os.system(ffmpeg_command)
 
-            processed_videos.append(video_path)
+            processed_videos.append(out_video_path[5:])
+
+            save_flist(processed_videos)
 
         # combine videos into final merged videofile
         save_flist(processed_videos)
 
         FFM_out = group["name"] + ".mp4"
 
-        # only supporte the same video_format, copy and not recode.
-        call = f'ffmpeg -f concat -safe 0 -i temp\\\\list.txt -c copy "{path}\\\\{FFM_out}" -y'
-
+        # only support the same video_format, copy and not recode.
+        call = f'ffmpeg -f concat -safe 0 -i temp\\list.txt -c copy "{path}\\\\{FFM_out}" -y'
         os.system(call)
 
         # set json group as finished merging
@@ -105,7 +95,5 @@ if __name__ == "__main__":
     channels = glob.glob(main_folder + "Channels/*")
 
     for channel in channels:
-        # print current channel
-        print(channel)
         # merge videos in channel
         merge_videos(channel)
